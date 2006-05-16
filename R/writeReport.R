efe89f141fe228354e313c77258445d5fc53c4e9
whuber <whuber@bc3139a8-67e5-0310-9ffc-ced21a209358>
2006-05-16 11:09:47 +0000

    updated writeReport to return full path and name of report index file, in response to a reviewer comment
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/cellHTS@18081 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/writeReport.R
+++ R/writeReport.R
@@ -69,7 +69,7 @@ writeHTMLtable4plots = function(x, con,
 
 
 
-writeReport = function(x, outdir=x$name, force=FALSE,
+writeReport = function(x, outdir=file.path(getwd(), x$name), force=FALSE,
   plotPlateArgs = FALSE, imageScreenArgs = NULL) {
  
   if(!inherits(x, "cellHTS"))
@@ -91,8 +91,9 @@ writeReport = function(x, outdir=x$name, force=FALSE,
   } else {
     dir.create(outdir, recursive=TRUE)
   }
-  
-  con = file(file.path(outdir, "index.html"), "w")
+
+  indexFile = file.path(outdir, "index.html") 
+  con = file(indexFile, "w")
   on.exit(close(con))
 
 
@@ -129,54 +130,54 @@ writeReport = function(x, outdir=x$name, force=FALSE,
   url = matrix(as.character(NA), nrow=nrow(exptab), ncol=ncol(exptab))
   colnames(url) = colnames(exptab)
   qmHaveBeenAdded = FALSE
-if (x$state["configured"]) {
-  for(p in 1:nrPlate){
-#    for(ch in 1:nrChannel){
+  if (x$state["configured"]) {
+    for(p in 1:nrPlate){
+      ##    for(ch in 1:nrChannel){
       nm = p
       wh = with(x$plateList, which(Plate==p & status=="OK"))
       if(length(wh)>0) {
         dir.create(file.path(outdir, nm))
-         if(x$state["normalized"]) {
+        if(x$state["normalized"]) {
           datPlat = x$xnorm[, p,,, drop=FALSE]
-  # datPlat = x$xnorm[, p,, ch, drop=FALSE]
+          ## datPlat = x$xnorm[, p,, ch, drop=FALSE]
           whatDat = "normalized"
         } else {
-  # datPlat = x$xraw[, p,, ch, drop=FALSE]
+          ## datPlat = x$xraw[, p,, ch, drop=FALSE]
           datPlat = x$xraw[, p,,, drop=FALSE]
           whatDat = "unnormalized"
         }
         res = QMbyPlate(datPlat, x$wellAnno[nrWell*(p-1)+(1:nrWell)], x$pdim, 
           name=sprintf("Plate %d (%s)", p, whatDat),
           basePath=outdir, subPath=nm, plotPlateArgs=plotPlateArgs, brks = brks, finalWellAnno = x$finalWellAnno[,p,,, drop=FALSE])
-
-
+        
+        
         url[wh, "status"] = res$url
         if(!qmHaveBeenAdded) {
-         #resChan = res$qmsummary[[1]]
-         #url = cbind(url,  matrix(as.character(NA), nrow=nrow(url), ncol=length(resChan)))
-         #for (j in names(resChan)) exptab[, j] = rep("", nrow(exptab))
-         #qmHaveBeenAdded = TRUE
+         ##resChan = res$qmsummary[[1]]
+         ##url = cbind(url,  matrix(as.character(NA), nrow=nrow(url), ncol=length(resChan)))
+         ##for (j in names(resChan)) exptab[, j] = rep("", nrow(exptab))
+         ##qmHaveBeenAdded = TRUE
          
          url = cbind(url,  matrix(as.character(NA), nrow=nrow(url), ncol=3))
          TableNames = c("Replicate dynamic range", "Average dynamic range", "Spearman rank correlation")
          for (j in TableNames) exptab[, j] = rep("", nrow(exptab))
           qmHaveBeenAdded = TRUE
-        }
-       whh = split(wh, exptab$Channel[wh])
-       for(ch in 1:length(res$qmsummary)) { # Channels
-        resCh = res$qmsummary[[ch]]
-        whCh = whh[[ch]]
-        exptab[whCh, "Replicate dynamic range"] = resCh[exptab$Replicate[whCh]]
-        exptab[whCh, "Average dynamic range"] = resCh[nrReplicate + 1]
-        exptab[whCh, "Spearman rank correlation"] = resCh[nrReplicate + 2]
-        #for(j in names(resCh)[(nrReplicate+1):) exptab[whCh, j] =resCh[j]
-} # channel
- } ## if
+       }
+        whh = split(wh, exptab$Channel[wh])
+        for(ch in 1:length(res$qmsummary)) { # Channels
+          resCh = res$qmsummary[[ch]]
+          whCh = whh[[ch]]
+          exptab[whCh, "Replicate dynamic range"] = resCh[exptab$Replicate[whCh]]
+          exptab[whCh, "Average dynamic range"] = resCh[nrReplicate + 1]
+          exptab[whCh, "Spearman rank correlation"] = resCh[nrReplicate + 2]
+          ## for(j in names(resCh)[(nrReplicate+1):) exptab[whCh, j] =resCh[j]
+        } # channel
+      } ## if
     }
-} # if configured
+  } # if configured
 
   ## Report pages per plate result file 
-  #dir.create(file.path(outdir, "in"))
+  ## dir.create(file.path(outdir, "in"))
   wh = which(x$plateList$status=="OK")
   nm = file.path("in", names(x$intensityFiles))
   for(w in wh) {
@@ -187,18 +188,19 @@ if (x$state["configured"]) {
     writeLines(txt, file.path(outdir, nm[w]))
     url[w, "Filename"] = nm[w]
   }
-
+  
   cat("<CENTER>", file=con)
   writeHTMLtable(exptab, url=url, con=con)
   cat("</CENTER><BR><BR>", file=con)
-
+  
   ## Per experiment QC
   plotTable = QMexperiment(x, outdir, con)
 
-
   ## Score table and screen-wide QC
+  ## To do (wh 16.5.2006): the code within this monster if-statement would probably better
+  ##   be encapsulated in a separate helper function?
   if(x$state["scored"]) {
-    # Checks whether the number of channels has changed after normalization
+    ## Checks whether the number of channels has changed after normalization
     trueNrCh = dim(x$xraw)[4]
     w=1:length(x$score)
     out=data.frame(
@@ -206,41 +208,50 @@ if (x$state["configured"]) {
       pos=1+(w-1)%%nrWell,
       score=x$score, wellAnno = x$wellAnno)
 
-## Include the normalized values
-for (ch in 1:nrChannel) out[sprintf("normalized_r%d_ch%d", 1:nrReplicate, ch)] = round(matrix(x$xnorm[,,,ch], nrow = nrWell*nrPlate, ncol = nrReplicate), 3)
+    ## Include the normalized values
+    for (ch in 1:nrChannel)
+      out[sprintf("normalized_r%d_ch%d", 1:nrReplicate, ch)] = round(matrix(x$xnorm[,,,ch], nrow = nrWell*nrPlate, ncol = nrReplicate), 3)
 
+    
+    ## include also the final well annotation (after the screen log file)
+    for (ch in 1:trueNrCh)
+      out[sprintf("finalWellAnno_r%d_ch%d", 1:nrReplicate, ch)] = matrix(x$finalWellAnno[,,,ch], nrow = nrWell*nrPlate, ncol = nrReplicate)
 
-         ## include also the final well annotation (after the screen log file)
-for (ch in 1:trueNrCh) out[sprintf("finalWellAnno_r%d_ch%d", 1:nrReplicate, ch)] = matrix(x$finalWellAnno[,,,ch], nrow = nrWell*nrPlate, ncol = nrReplicate)
+    ## include also the raw values for each replicate and channel	 
+    for (ch in 1:trueNrCh)
+      out[sprintf("raw_r%d_ch%d", 1:nrReplicate, ch)] = matrix(x$xraw[,,,ch], nrow = nrWell*nrPlate, ncol = nrReplicate)
 
-         ## include also the raw values for each replicate and channel	 
-for (ch in 1:trueNrCh) out[sprintf("raw_r%d_ch%d", 1:nrReplicate, ch)] = matrix(x$xraw[,,,ch], nrow = nrWell*nrPlate, ncol = nrReplicate)
-
-         # median between replicates  
-for (ch in 1:trueNrCh) {
- if (nrReplicate > 1) {
-            out[sprintf("median_ch%d", ch)] = apply(out[sprintf("raw_r%d_ch%d", 1:nrReplicate, ch)],1,median)
-            if (nrReplicate ==2) { 
-           # Difference between replicates
-	   out[sprintf("diff_ch%d", ch)] = apply(out[sprintf("raw_r%d_ch%d", 1:nrReplicate, ch)], 1, diff)} else {
-         # average between replicates
-	 out[sprintf("average_ch%d", ch)] = apply(out[sprintf("raw_r%d_ch%d", 1:nrReplicate, ch)], 1, mean)}} }
-  	 # raw/plateMedian
-	 xn = array(as.numeric(NA), dim=dim(x$xraw))
-         for(p in 1:nrPlate) {
-            samples = (x$wellAnno[(1:nrWell)+nrWell*(p-1)]=="sample")
-            for(r in 1:nrReplicate)
-              for (ch in 1:trueNrCh) 
-                xn[, p, r, ch] = x$xraw[, p, r, ch] / median(x$xraw[samples, p, r, ch], na.rm=TRUE)
-         }
-for (ch in 1:trueNrCh) out[sprintf("raw/PlateMedian_r%d_ch%d", 1:nrReplicate, ch)] = signif(matrix(xn[,,,ch], nrow = nrWell*nrPlate, ncol = nrReplicate), 3)
-
+    ## median between replicates  
+    for (ch in 1:trueNrCh) {
+      if (nrReplicate > 1) {
+        out[sprintf("median_ch%d", ch)] = apply(out[sprintf("raw_r%d_ch%d", 1:nrReplicate, ch)],1,median)
+        if (nrReplicate ==2) { 
+          ## Difference between replicates
+          out[sprintf("diff_ch%d", ch)] = apply(out[sprintf("raw_r%d_ch%d", 1:nrReplicate, ch)], 1, diff)
+        } else {
+          ## average between replicates
+          out[sprintf("average_ch%d", ch)] = apply(out[sprintf("raw_r%d_ch%d", 1:nrReplicate, ch)], 1, mean)
+        } ## if
+      } ## if
+    } ## for ch
+    ## raw/plateMedian
+    xn = array(as.numeric(NA), dim=dim(x$xraw))
+    for(p in 1:nrPlate) {
+      samples = (x$wellAnno[(1:nrWell)+nrWell*(p-1)]=="sample")
+      for(r in 1:nrReplicate)
+        for (ch in 1:trueNrCh) 
+          xn[, p, r, ch] = x$xraw[, p, r, ch] / median(x$xraw[samples, p, r, ch], na.rm=TRUE)
+    }
+    for (ch in 1:trueNrCh)
+      out[sprintf("raw/PlateMedian_r%d_ch%d", 1:nrReplicate, ch)] = signif(matrix(xn[,,,ch], nrow = nrWell*nrPlate, ncol = nrReplicate), 3)
 
     if(x$state["annotated"]) {
-          out = cbind(out, x$geneAnno)
-          out = out[,!duplicated(tolower(names(out)))] 
-	  ttInfo = "Table of scored <BR> and annotated probes"
-          } else ttInfo = "Table of scored probes"
+      out = cbind(out, x$geneAnno)
+      out = out[,!duplicated(tolower(names(out)))] 
+      ttInfo = "Table of scored <BR> and annotated probes"
+    } else {
+      ttInfo = "Table of scored probes"
+    }
     ## consider only the wells with sample and controls, at least for one of the replicates
     toconsider = which(!apply(out[,grep("finalWellAnno",names(out))], 1, function(u) all(u=="flagged") || any(u=="empty") || any(u=="other")))
     out = out[toconsider, ]
@@ -248,20 +259,20 @@ for (ch in 1:trueNrCh) out[sprintf("raw/PlateMedian_r%d_ch%d", 1:nrReplicate, ch
     out$score = round(out$score, 2)
     write.table(out, file=file.path(outdir, "topTable.txt"), sep="\t", row.names=FALSE, col.names=TRUE, quote = FALSE)
 
-
     ## screen-wide QC (image plot with the z score values)
     makePlot(outdir, con=con, name="imageScreen", w=7, h=7, psz=6,
              fun = function() do.call("imageScreen", args=append(list(x=x), imageScreenArgs)), print=FALSE)
+    
 
+    count = nrow(plotTable)
+    plotTable = rbind(plotTable, rep("", length=prod(ncol(plotTable)* 2))) 
+    plotTable[count + 1, 2] = "<H3 align=center>Screen-wide image plot of the scored values</H3>"
+    plotTable[count + 2, 1] = sprintf("<CENTER><A HREF=\"topTable.txt\"><em>%s</em></A></CENTER><BR>\n", ttInfo)
+    plotTable[count + 2, 2] = sprintf("<CENTER><A HREF=\"%s\"><IMG SRC=\"%s\"/></A></CENTER><BR>\n", "imageScreen.pdf", "imageScreen.png") 
+  } ## if scored
 
-count = nrow(plotTable)
-plotTable = rbind(plotTable, rep("", length=prod(ncol(plotTable)* 2))) 
-plotTable[count + 1, 2] = "<H3 align=center>Screen-wide image plot of the scored values</H3>"
-plotTable[count + 2, 1] = sprintf("<CENTER><A HREF=\"topTable.txt\"><em>%s</em></A></CENTER><BR>\n", ttInfo)
-plotTable[count + 2, 2] = sprintf("<CENTER><A HREF=\"%s\"><IMG SRC=\"%s\"/></A></CENTER><BR>\n", "imageScreen.pdf", "imageScreen.png") 
-}
-
-writeHTMLtable4plots(plotTable, con=con)
+  writeHTMLtable4plots(plotTable, con=con)
+  
   writetail(con)
-  return(outdir)
+  return(indexFile)
 }
