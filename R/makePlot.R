8c6a3d387acc07b9eaaec470b4d93e08ee3a68a2
whuber <whuber@bc3139a8-67e5-0310-9ffc-ced21a209358>
2006-06-26 08:17:59 +0000

    fixed a bug in write.tabdel example there is still a problem with the call to PDF in makePlot
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/cellHTS@18617 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/makePlot.R
+++ R/makePlot.R
@@ -1,13 +1,18 @@
 makePlot = function(path, con, name, w, h, fun, psz=12, print=TRUE) {
-  outfiles = paste(name, c("pdf", "png"), sep=".")
+
+  outf = paste(name, c("pdf", "png"), sep=".")
   nrppi = 72
-  for(out in seq(along=outfiles)) {
-    switch(out,
-           pdf(file.path(path, outfiles[1]), width=w, height=h, pointsize=psz),
-           png(file.path(path, outfiles[2]), width=w*nrppi, height=h*nrppi, pointsize=psz))
-    fun()
-    dev.off()
-  }
-if (print) cat(sprintf("<CENTER><A HREF=\"%s\"><IMG SRC=\"%s\"/></A></CENTER><BR>\n", outfiles[1], outfiles[2]), file=con)
+
+  pdf(file.path(path, outf[1]), width=w, height=h, pointsize=psz)
+  fun()
+  dev.off()
+  
+  png(file.path(path, outf[2]), width=w*nrppi, height=h*nrppi, pointsize=psz)
+  fun()
+  dev.off()
+
+  if (print)
+    cat(sprintf("<CENTER><A HREF=\"%s\"><IMG SRC=\"%s\"/></A></CENTER><BR>\n",
+                outf[1], outf[2]), file=con)
 }
 
