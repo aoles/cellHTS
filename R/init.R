b99ac0c4c6984db814c33d6fa2071941bb122b1b
whuber <whuber@bc3139a8-67e5-0310-9ffc-ced21a209358>
2006-08-05 20:40:05 +0000

    better welcome message
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/cellHTS@19049 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/init.R
+++ R/init.R
@@ -1,6 +1,7 @@
 .onLoad <- function(lib, pkg) {
   ## this is only for the package version that will go to the Genome Biology website
   ##  shall be removed subsequently
-  cat(sprintf("\n\n\t\tcellHTS %s\nPlease check at www.bioconductor.org for the most recent version.\n\n",
-       packageDescription("cellHTS")$Version))
+  pDesc = packageDescription("cellHTS")
+  cat(sprintf("\n\n\t\tcellHTS %s, %s\nPlease check at www.bioconductor.org for the most recent version.\n\n",
+      pDesc$Version, pDesc$Date))
 }
