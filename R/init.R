873279dd4e13c68f6aa0d2b39765a247b91d190c
whuber <whuber@bc3139a8-67e5-0310-9ffc-ced21a209358>
2006-08-13 21:55:39 +0000

    removed the blurb at .onLoad
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/cellHTS@19179 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/init.R
+++ R/init.R
@@ -1,7 +1,5 @@
 .onLoad <- function(lib, pkg) {
-  ## this is only for the package version that will go to the Genome Biology website
-  ##  shall be removed subsequently
-  pDesc = packageDescription("cellHTS")
-  cat(sprintf("\n\n\t\tcellHTS %s, %s\nPlease check at www.bioconductor.org for the most recent version.\n\n",
-      pDesc$Version, pDesc$Date))
+  ## pDesc = packageDescription("cellHTS")
+  ## cat(sprintf("\n\n\t\tcellHTS %s, %s\nPlease check at www.bioconductor.org for the most recent version.\n\n",
+  ## pDesc$Version, pDesc$Date))
 }
