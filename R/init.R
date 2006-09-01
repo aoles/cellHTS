311449e3b5d388c6024aa3e973a6f66ad613ba33
whuber <whuber@bc3139a8-67e5-0310-9ffc-ced21a209358>
2006-09-01 21:57:44 +0000

    fixed typo in man page and added getMatrix to exports in Namespace
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/cellHTS@19577 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/init.R
+++
@@ -1,5 +0,0 @@
-.onLoad <- function(lib, pkg) {
-  ## pDesc = packageDescription("cellHTS")
-  ## cat(sprintf("\n\n\t\tcellHTS %s, %s\nPlease check at www.bioconductor.org for the most recent version.\n\n",
-  ## pDesc$Version, pDesc$Date))
-}
