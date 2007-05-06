42fb9864c82c86a79e191c137e1feede0b3314bb
whuber <whuber@bc3139a8-67e5-0310-9ffc-ced21a209358>
2007-05-06 10:11:32 +0000

    The plate normalization in normalizeChannels.R is now done BEFORE calling fun. I have also simplified its code and man page.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/cellHTS@24518 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/calcZscores.R
+++ R/calcZscores.R
@@ -1,19 +1,22 @@
 calcZscores = function(x, sign="+", one) {
-  samps = (x$wellAnno=="sample")
+
   sg = switch(sign,
     "+" = 1,
     "-" = -1,
-    stop(sprintf("Invalid value '%s' for argument 'zscore'", sign)))
+    stop(sprintf("Invalid value '%s' for argument 'sign'", sign)))
 
   xn = x$xnorm
 
-  for (ch in 1:(dim(xn)[4])) {
-    if (!missing(one))
-      stopifnot(apply(xn[,,,ch,drop=FALSE], 3, function(z) abs(median(z[samps], na.rm=TRUE) - one) < 1e-10))
-
-    xn[,,,ch] = apply(xn[,,,ch,drop=FALSE], 3, function(z)
-        sg*(z-median(z[samps], na.rm=TRUE))/mad(z[samps], na.rm=TRUE))
+  for(ch in 1:(dim(xn)[4])) {
+    for(r in 1:(dim(xn)[3])) {
+      v = xn[,,r,ch,drop=FALSE]
+      sv = v[(x$wellAnno=="sample")]
+      med = median(sv, na.rm=TRUE)
+      if (!missing(one))
+        stopifnot(abs(med-one) < 1e-10)
+      xn[,,r,ch] = sg*(v-med)/mad(sv, na.rm=TRUE)
+    }
   }
-  dim(xn) = dim(x$xnorm)
+  
   return(xn)
 }
--- R/normalizeChannels.R
+++ R/normalizeChannels.R
@@ -1,4 +1,15 @@
-normalizeChannels = function(x, fun=function(r1,r2) r2/r1, log=FALSE, adjustPlates, zscore, posControls, negControls, ...){
+##
+## Fixmes (wh 6.5.2007):
+## 1. Currently, I think that the normalization (adjustPlates) should be done
+## BEFORE the summarization (fun).
+## Perhaps it would be good to have an option to call it either before or
+## after (or both?). Not sure...
+##
+##
+normalizeChannels = function(x,
+    fun = function(r1, r2, thresh) ifelse(r1>thresh, log2(r2/r1), -Inf),
+    funargs = list(thresh=0.7),
+    adjustPlates, zscore, ...){
 
   if(!x$state["configured"])
     stop("Please configure 'x' (using the function 'configure.cellHTS') before normalization.")
@@ -6,45 +17,30 @@ normalizeChannels = function(x, fun=function(r1,r2) r2/r1, log=FALSE, adjustPlat
   if(dim(x$xraw)[4] != 2)
     stop("Currently this function is implemented only for dual-channel data.")
 
-  xn = array(as.numeric(NA), dim=c(dim(x$xraw)[-4], 1))
-  nrWpP = dim(x$xraw)[1]
-
-  ## The argument 'fun' allows using different normalizations, and also to define the numerator/denominator for the ratio (i.e. R1/R2 or R2/R1)
-  for(p in 1:(dim(x$xraw)[2])) {
-    for(r in 1:(dim(x$xraw)[3])) 
-          xn[, p, r, 1] = fun(x$xraw[, p, r, 1], x$xraw[, p, r, 2])  
-  }
-
-  ## log2 transformes the result of 'fun'
-  if (log) xn = log2(xn)
-
-  x$xnorm = xn
-
+  ## Apply the chosen plate-wise normalization function
   if (!missing(adjustPlates)) {
-
-    ## Apply the chosen plate-wise normalization function
     x = switch(adjustPlates,
-      mean = scaleByPlateMean(x, what="xnorm", isInLogScale=log),
-      median = scaleByPlateMedian(x, what="xnorm", isInLogScale=log),
-      shorth = scaleByPlateShorth(x, what="xnorm", isInLogScale=log),
-      negatives = scaleByPlateNegatives(x, negControls, what="xnorm", isInLogScale=log),
-      POC = POC(x, posControls, what="xnorm"),
-      NPI = NPI(x, posControls, negControls, what="xnorm"),
-      Bscore = Bscore(x, what="xnorm", ...),  
+      mean = scaleByPlateMean(x),
+      median = scaleByPlateMedian(x),
+      shorth = scaleByPlateShorth(x),
+      negatives = scaleByPlateNegatives(x),
+      POC = POC(x, ...),
+      NPI = NPI(x, ...),
+      Bscore = Bscore(x, ...),  
       stop(sprintf("Invalid value '%s' for argument 'adjustPlates'", adjustPlates)))
-  }
 
-  ## calculates the z-score for each replicate separately
-  if(!missing(zscore)) {
-  samps = (x$wellAnno=="sample")
-  sg = switch(zscore,
-    "+" = 1,
-    "-" = -1,
-    stop(sprintf("Invalid value '%s' for argument 'zscore'", zscore)))
+    x$xraw = x$xnorm
+  } 
 
-    for(r in 1:(dim(xn)[3]))
-     x$xnorm[, , r, 1] = sg * (x$xnorm[, , r, 1] - median(x$xnorm[,, r, 1][samps], na.rm=TRUE)) / mad(x$xnorm[,, r, 1][samps], na.rm=TRUE)
-   }
+  ## The argument 'fun' allows using different normalizations, and also to define
+  ## the numerator/denominator for the ratio (i.e. R1/R2 or R2/R1)
+  x$xnorm = array(
+     do.call("fun", append(list(r1=x$xraw[,,,1], r2=x$xraw[,,,2]), funargs)),
+            dim=c(dim(x$xraw)[1:3], 1))
+  
+  ## calculates the z-scores, separately for each replicate
+  if(!missing(zscore))
+    x$xnorm = calcZscores(x, sign=zscore) 
 
   x$state["normalized"] = TRUE
   return(x)
