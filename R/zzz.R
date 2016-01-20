f3b9b79dcbfd0697c820093abdd3ff218f094478
a.oles <a.oles@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-01-20 17:28:36 +0000

    Deprecate package
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/cellHTS@112745 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -7,16 +7,14 @@
    if( .Platform$OS.type == "windows" && .Platform$GUI == "Rgui" )
       addVigs2WinMenu("cellHTS") ## in Biobase
 
-   if(interactive())
-     packageStartupMessage(paste(
-   "\n-------------------------------------------------------------------",
-   "\nA note from the 'cellHTS' package:",
-   "\nThe package 'cellHTS2' offers better functionality for working with",
-   "\nmultiple screens and with multi-channel screens. Please consider",
-   "\nusing 'cellHTS2' for new projects. However, 'cellHTS' will be",
-   "\nsupported for a while to help with your existing projects.",
-   "\n-------------------------------------------------------------------\n\n",
-      sep=""))
-
+    msg <- sprintf(
+      "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s.", pkgname, "3.4")
+    
+    msg <- paste(msg, 
+      "Please consider using 'cellHTS2' which offers better functionality
+        for working with multiple screens and with multi-channel screens.",
+      sep = "\n")
+    
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
 }
-
